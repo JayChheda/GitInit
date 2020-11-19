e24c80f332cc4e2a3c391c70e7f9f0cac2c609f9
JayChheda <32122695+JayChheda@users.noreply.github.com>
2020-11-19 17:17:29 +0530

    Local Project

---
+++ src/test/java/com/lti/SampleApp1/AppTest.java
@@ -0,0 +1,30 @@
+package com.lti.SampleApp1;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    public void testApp()
+    {
+        assertTrue( true );
+        App app = new App();
+        app.main(null);
+        System.out.println("Welcome");
+    }
+}
